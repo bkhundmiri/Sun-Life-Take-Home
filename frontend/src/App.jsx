41aebc9120e9f06cb6e919f860dfacf0de8d180f
Bilal Khundmiri <khundmirib@gmail.com>
2023-06-02 14:54:14 -0500

    added frontend and minimal styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
+import AllStatus from './components/all-status/allStatus.jsx'
+import SingleStatus from './components/single-status/singleStatus.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <header className="app-header">
+        <h2>Status Check</h2>
+      </header>
+
+      <main>
+        <div className="single-status-container">
+          <SingleStatus url="/api/v1/google-status" name="Google" />
+          <SingleStatus url="/api/v1/amazon-status" name="Amazon" />
+        </div>
+        <AllStatus />
+      </main>
+
+      <footer className="app-footer">
+        <p>Bilal Khundmiri, made for Sun Life</p>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+
